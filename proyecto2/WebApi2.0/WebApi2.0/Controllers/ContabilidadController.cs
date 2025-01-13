906c32e7765404db64bcbdbb91ab04dcdcaae5bd
Miguel Herrera <121081107+MHerrera94@users.noreply.github.com>
2025-01-12 20:09:51 -0500

    datos nuevos

---
+++ proyecto2/WebApi2.0/WebApi2.0/Controllers/ContabilidadController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2._0.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+        // GET: ContabilidadController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ContabilidadController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ContabilidadController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContabilidadController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContabilidadController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ContabilidadController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContabilidadController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ContabilidadController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- proyecto2/WebApi2.0/WebApi2.0/obj/Debug/net8.0/WebApi2.0.AssemblyInfo.cs
+++ proyecto2/WebApi2.0/WebApi2.0/obj/Debug/net8.0/WebApi2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e7bbe9290d65c3cb8d614e7fe03fe69a6fd4fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8be5cbcf4140f66186d8d3eeef3fc84c4cce5318")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
